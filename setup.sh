e53fdd276f59c43916bf6a34ca0c29ed47693818
Wytamma Wirth <wytamma.wirth@me.com>
2018-06-23 20:27:26 +1000

    no debug

--- setup.sh
+++ setup.sh
@@ -7,21 +7,22 @@ conda config --add channels r
 conda config --add channels bioconda
 
 # create conda env
-conda create --yes --name virusMAP 
+conda create --yes --name virusMAP python=3.6
 
 #install dependancies
-echo('Installing dependancies, this will take a while...')
-echo('I turned on debug so you have something to watch ;)')
+echo '**************************************************'
+echo 'Installing dependancies, this will take a while...'
+echo '**************************************************'
 
 sleep 3
 
-conda install --yes --debug -n virusMAP biopython=1.70 
-conda install --yes --debug -n virusMAP fastqc=0.11.7
-conda install --yes --debug -n virusMAP snakemake=5.1.4
-conda install --yes --debug -n virusMAP trim-galore=0.4.5
-conda install --yes --debug -n virusMAP star=2.6.0c 
-conda install --yes --debug -n virusMAP qualimap=2.2.2a
-conda install --yes --debug -n virusMAP multiqc=1.5
+conda install --yes -n virusMAP biopython=1.70 
+conda install --yes -n virusMAP fastqc=0.11.7
+conda install --yes -n virusMAP snakemake=5.1.4
+conda install --yes -n virusMAP trim-galore=0.4.5
+conda install --yes -n virusMAP star=2.6.0c 
+conda install --yes -n virusMAP qualimap=2.2.2a
+conda install --yes -n virusMAP multiqc=1.5
 
-echo('To activate the virusMAP environment, use')
-echo(' $ conda activate virusMAP')
+echo 'To activate the virusMAP environment, use'
+echo ' $ conda activate virusMAP' 
