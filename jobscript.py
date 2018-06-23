164619e28fb8bfa402935ee77807ad269184fbd1
Wytamma Wirth <wytamma.wirth@me.com>
2018-06-23 23:26:44 +1000

    removed threads

--- jobscript.py
+++ jobscript.py
@@ -9,14 +9,17 @@ from snakemake.utils import read_job_properties
 jobscript = sys.argv[1]
 job_properties = read_job_properties(jobscript)
 
+"""
 # do something useful with the threads
 if threads in job_properties:
     threads = job_properties[threads]
 else:
     threads = 1
+"""
 
 # access property defined in the cluster configuration file (Snakemake >=3.6.0)
-job_properties["cluster"]["time"]
+#job_properties["cluster"]["time"]
+
 
 os.system(
     "qsub -v PATH='/homes/22/jc220896/miniconda3/envs/virusMAP/bin:$PATH' \
@@ -24,7 +27,7 @@ os.system(
     -o /homes/22/jc220896/virusMAP/qsublogs/ \
     -e /homes/22/jc220896/virusMAP/qsublogs/ \
     -l pmem=2GB \
-    -l nodes=1:ppn={threads} \
+    -l nodes=1:ppn=1 \
     {script}".format(
-       script=jobscript)
+        script=jobscript)
     )
