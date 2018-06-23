82fa3c9722579e9b786930bec6cd9fb0ac4c30b8
Wytamma Wirth <wytamma.wirth@me.com>
2018-06-23 23:36:44 +1000

    escaped the vairables

--- jobscript.py
+++ jobscript.py
@@ -9,13 +9,12 @@ from snakemake.utils import read_job_properties
 jobscript = sys.argv[1]
 job_properties = read_job_properties(jobscript)
 
-"""
+
 # do something useful with the threads
 if threads in job_properties:
     threads = job_properties[threads]
 else:
     threads = 1
-"""
 
 # access property defined in the cluster configuration file (Snakemake >=3.6.0)
 #job_properties["cluster"]["time"]
@@ -23,9 +22,6 @@ else:
 
 os.system(
     """qsub -v PATH='/homes/22/jc220896/miniconda3/envs/virusMAP/bin:$PATH' \
-    -d /homes/22/jc220896/virusMAP/ \
-    -o /homes/22/jc220896/virusMAP/qsublogs/ \
-    -e /homes/22/jc220896/virusMAP/qsublogs/ \
     -l pmem=2GB \
-    -l nodes=1:ppn=1 \
-    {{jobscript}}""".format(jobscript=jobscript)
+    -l nodes=1:ppn={{threads}} \
+    {{jobscript}}""".format(jobscript=jobscript, threads=threads)
