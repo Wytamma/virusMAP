87ce43b3f59df3fd1591051bee8041be4e6ac153
Wytamma Wirth <wytamma.wirth@me.com>
2018-06-23 20:51:33 +1000

    docs

--- setup.sh
+++ setup.sh
@@ -10,10 +10,11 @@ conda config --add channels bioconda
 conda create --yes --name virusMAP python=3.6
 
 #install dependancies
+echo ''
 echo '**************************************************'
 echo 'Installing dependancies, this will take a while...'
 echo '**************************************************'
-
+echo ''
 sleep 3
 
 conda install --yes -n virusMAP biopython=1.70 
@@ -24,5 +25,9 @@ conda install --yes -n virusMAP star=2.6.0c
 conda install --yes -n virusMAP qualimap=2.2.2a
 conda install --yes -n virusMAP multiqc=1.5
 
+echo ''
+echo '**************************************************'
 echo 'To activate the virusMAP environment, use'
 echo ' $ conda activate virusMAP' 
+echo '**************************************************'
+echo ''
