2f047f6f689cf7214ae265d89219078ecba5a049
Wytamma Wirth <wytamma.wirth@me.com>
2018-06-23 18:53:08 +1000

    manual install

--- setup.sh
+++ setup.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/bash
 
-# create conda env from environment.yml
-conda env create -n virusMAP -f environment.yml
+# create conda env
+conda create --yes --name virusMAP 
 
 # activate conda env
 source activate virusMAP
+
+# add channels
+conda config --add channels conda-forge
+conda config --add channels defaults
+conda config --add channels r
+conda config --add channels bioconda
+
+#install dependancies
+conda install biopython=1.70 trim-galore=0.4.5 snakemake=5.1.4 star=2.6.0c fastqc=0.11.7 qualimap=2.2.2a multiqc=1.5
